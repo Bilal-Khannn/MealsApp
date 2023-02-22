aebb3d0593f3e67ea17338dd27c7267188fbec53
Bilal <bkhan8423@gmail.com>
2023-02-22 06:12:57 +0000

    added data fetching for meals

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,9 @@
+import { useGlobalContext } from "../context";
+
 export default function Meals() {
+  const context = useGlobalContext();
+
+  console.log(context);
   return (
     <div>
       <h1>Meals</h1>
--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,31 @@
-import React, { useContext } from "react";
-
+import React, { useContext, useEffect } from "react";
+import axios from "axios";
 const AppContext = React.createContext();
 
+const allMealsUrl =
+  "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
+
+const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+
 const AppProvider = ({ children }) => {
+  async function fetchMeals(url) {
+    try {
+      const { data } = await axios(url);
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+
+  useEffect(() => {
+    fetchMeals(allMealsUrl);
+  }, []);
+
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
 };
 
 export { AppContext, AppProvider };
